c894267d287002bdbf8f3664bbb2d16cf1179c1f
Richard Grec <Richardgrec@icloud.com>
2022-04-28 23:07:40 +0200

    WIP Occupation dashboard widgets

---
+++ components/GymWidget.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card class="gym-widget-card text-center">
+    <v-card-title class="title">
+      Gym {{ number }}
+    </v-card-title>
+    <v-card-subtitle class="subtitle">
+      Door {{ doorNumber }}
+    </v-card-subtitle>
+    <v-divider />
+    <v-card-text class="text-center occupation" :class="colorClass">
+      {{ occupation }}/{{ capacity }}
+    </v-card-text>
+    <v-card-actions class="justify-end">
+      <v-btn class="info-button primary">
+        Info
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'GymWidget',
+  props: {
+    number: {
+      type: Number,
+      default: 0
+    },
+    doorNumber: {
+      type: String,
+      default: 'XXX'
+    },
+    occupation: {
+      type: Number,
+      default: -1
+    },
+    capacity: {
+      type: Number,
+      default: -1
+    }
+  },
+  computed: {
+    colorClass () {
+      if (this.occupation === 0) {
+        return 'green--text'
+      }
+      if (this.occupation < this.capacity) {
+        return 'yellow--text'
+      }
+      if (this.occupation === this.capacity) {
+        return 'red--text'
+      }
+      return 'blue--text'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.gym-widget-card {
+  margin: 0 5em;
+  .title {
+    font-weight: 700;
+  }
+  .subtitle {
+    font-weight: 700;
+  }
+  .occupation {
+    font-size: 72px;
+    padding: 1em 0.30em;
+  }
+  .info-button {
+    margin: 0.5em 1.5em;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="10" md="8">
       <h1>Dashboard</h1>
-      <p>Welcome</p>
-      <v-btn @click="logout">
+
+      <v-row justify="center" align="center">
+        <v-col>
+          <GymWidget :number="1" :door-number="'012'" :occupation="2" :capacity="5" />
+        </v-col>
+        <v-col>
+          <GymWidget :number="2" :door-number="'013'" :occupation="0" :capacity="4" />
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col>
+          <GymWidget :number="3" :door-number="'014'" :occupation="4" :capacity="4" />
+        </v-col>
+        <v-col>
+          <GymWidget :number="4" :door-number="'015'" :occupation="3" :capacity="6" />
+        </v-col>
+      </v-row>
+
+      <v-btn style="margin-top: 3em" @click="logout">
         Logout
       </v-btn>
     </v-col>
@@ -24,5 +41,6 @@ export default {
 <style lang="scss" scoped>
 h1 {
   color: #000;
+  margin-bottom: 2em;
 }
 </style>
